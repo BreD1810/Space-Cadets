b9c1c6b7cc2ec0dd0b7dc5040aee71f4bf4c8236
BreD1810 <32489229+BreD1810@users.noreply.github.com>
2017-10-14 16:42:17 +0100

    Initial Commit

---
+++ Week 2/src/Commands.java
@@ -0,0 +1,11 @@
+
+public class Commands {
+
+	public String command;
+	public String operand;
+	
+	Commands(String inputCommand, String inputOperand) {
+		command = inputCommand;
+		operand = inputOperand;
+	}
+}
